ab383a4b7ad34c2bbc2b9c9540a7a069f92a7fc3
rah643 <122642230+rah643@users.noreply.github.com>
2024-08-13 14:35:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+// windows.alert("hello js");
+// acc568708cfc5bf8e798384572bb6aa2acc568708cfc5bf8e798384572bb6aa2
+//https://api.openweathermap.org/data/2.5/weather?units=metric&q=Lucknow&appid=acc568708cfc5bf8e798384572bb6aa2
+
+async function weather(){
+    let city=document.querySelector('#search>input').value;
+    console.log(city);
+    
+    const url="https://api.openweathermap.org/data/2.5/weather?units=metric&q="+city+"&appid=0fde24f18909b27ce870124bbcdf96d5";
+    console.log(url)
+    const res= (await fetch(url));
+    const data=await res.json();
+    console.log(data);
+    temp.innerHTML=Math.floor(data["main"]["temp"]) +"°C";
+    cityname.innerHTML=data.name;
+    humid.innerHTML=data.main.humidity+"%";
+    wind.innerHTML=data.wind.speed+"KM/H";
+    console.log(data.weather[0].main);
+    
+    // console.log( (data.weather[0].main)==="Clear");
+    // const l=document.querySelector("#status").src;
+    // console.log(l);
+    
+
+    if((data.weather[0].main)==="Mist"){
+        
+        document.querySelector('#status').src="../Img/images/mist.png";
+    }
+    else if((data.weather[0].main)==="Clouds"){
+        
+        document.querySelector('#status').src="../Img/images/clouds.png";
+
+    }
+    else if((data.weather[0].main)==="Clear"){
+       
+        
+        document.querySelector('#status').src="../Img/images/clear.png";
+       
+    }
+else if((data.weather[0].main)==="Rain"){
+    document.querySelector('#status').src="../Img/images/rain.png";
+
+}
+    else if((data.weather[0].main)==="Snow"){
+        document.querySelector('status').src="../Img/images/snow.png";
+
+    }
+    else if((data.weather[0].main)==="Haze"){
+        document.querySelector('#status').src="../Img/images/haze.png";
+
+
+    }
+    else {
+        document.querySelector('#status').src="../Img/images/clear.png";
+
+    }
+
+}
